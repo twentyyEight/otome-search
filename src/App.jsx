d3b624098175d25f21a47f49639f310bf9a0584c
Paloma <paloma.cabrillana@gmail.com>
2025-06-19 15:03:24 -0400

    commit 1 - visual novels fetch & filters

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import VNList from "./components/VisualNovelsList"
+import Test from "./components/Test"
+
+function App() {
+
+  return (
+    <>
+      <Test />
+    </>
+  )
+}
+
+export default App
