8d128c326d2d3085ab84a2347ebac1dd96ee97d9
Paloma <paloma.cabrillana@gmail.com>
2025-06-25 12:48:25 -0400

    commit 5 - responsive in visual novel page

--- src/pages/VisualNovelPage.jsx
+++ src/pages/VisualNovelPage.jsx
@@ -12,10 +12,10 @@ export default function VisualNovelPage() {
             {loading && <h3>Loading...</h3>}
 
             {!loading &&
-                <div className="flex-col sm:flex-row flex items-center bg-white rounded-md w-[320px] sm:w-[1000px] sm:h-fit p-5">
+                <div className="flex-col sm:flex-row flex items-start bg-white rounded-md w-[330px] sm:w-[600px] md:w-[750px] lg:w-[1000px] sm:h-max p-5">
                     <img src={image?.url} alt="image" className="w-full sm:w-[320px] h-[400px] object-cover rounded-md" />
 
-                    <div className="h-[400px] sm:ml-4">
+                    <div className="sm:ml-4">
                         <h1 className="lato-bold text-2xl mt-3 sm:mt-0">{title}</h1>
                         <div className="flex">
                             {developers.map(dev =>
